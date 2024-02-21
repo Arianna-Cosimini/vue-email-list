a16931386867ebe42e74da06ed9c349f8538cdc8
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-21 16:45:08 +0100

    bonus: inserito controllo per stampare le emails tutte insieme

--- js/script.js
+++ js/script.js
@@ -4,25 +4,35 @@ createApp({
     data() {
         return {
 
-           emails: [],
+            emails: [],
+            emailGenerated:0,
         }
 
 
     },
 
-    mounted() {
-            for(let i = 0; i < 10; i++){
-
-                axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(email => {
-                    this.emails.push(email.data.response)
-                    console.log(this.emails)
-                });
-            }
-    },
+    // mounted() {
+    //     this.generateEmails()
+    // },
 
     methods: {
-       
+        generateEmails() {
+            for (let i = 0; i < 10; i++) {
+
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(email => {
+
+
+
+
+                        this.emails.push(email.data.response)
+                        this.emailGenerated++
+
+                        
+                    });
+            }
+
+        }
     }
 }).mount('#app');
