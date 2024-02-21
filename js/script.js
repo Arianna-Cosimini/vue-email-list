bfae7a34fb228bdf563d67849393d655f088f16b
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-21 17:07:36 +0100

    Esercizio commentato e finito

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            // array per inserire le mails generate
             emails: [],
+            // creo variabile per tenere traccia delle mails generate
             emailGenerated:0,
         }
 
@@ -14,16 +15,17 @@ createApp({
 
     methods: {
         generateEmails() {
+            // ciclo for per generare 10 mails
             for (let i = 0; i < 10; i++) {
 
                 axios
                     .get('https://flynn.boolean.careers/exercises/api/random/mail')
                     .then(email => {
-
-
-
-
+                        // inserisco email generate nell'array
                         this.emails.push(email.data.response)
+
+                        // aumento il numero di email generate fino a quando gli elementi
+                        // dell'array non saranno 10
                         this.emailGenerated++
 
                         
