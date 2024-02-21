cb0e1e234b381efee83ecbfffbd2c77f94da2bf7
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-21 15:44:24 +0100

    creato mounted con collegamento api e stampato email in pagina

--- js/script.js
+++ js/script.js
@@ -3,12 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+
+            emailCasuale: "",
         }
 
 
     },
 
-    methods: {
+    mounted() {
+        
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(emails => {
+                console.log(emails)
+                this.emailCasuale = emails.data.response
+            });
+    },
 
+    methods: {
+       
     }
 }).mount('#app');
