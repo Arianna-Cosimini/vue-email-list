618a0064d32597f20937d13733c1162ec59bd7d2
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-21 15:53:12 +0100

    creato ciclo for per generare 10 email in mounted

--- js/script.js
+++ js/script.js
@@ -4,20 +4,22 @@ createApp({
     data() {
         return {
 
-            emailCasuale: "",
+            emailCasuale: [],
         }
 
 
     },
 
     mounted() {
-        
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(emails => {
-                console.log(emails)
-                this.emailCasuale = emails.data.response
-            });
+            for(let i = 0; i < 10; i++){
+
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(emails => {
+                    console.log(emails)
+                    this.emailCasuale = emails.data.response
+                });
+            }
     },
 
     methods: {
